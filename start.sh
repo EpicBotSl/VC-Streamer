2f929637280e3110eda72f6b5ceee61f605c43f7
Supun Max <85874927+maxsupun@users.noreply.github.com>
2022-07-23 12:47:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/ZauteKm/VideoPlayerBot.git /VideoPlayerBot
+git clone -b master https://github.com/maxsupun/VC-Streamer.git /VideoPlayerBot
 cd /VideoPlayerBot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
