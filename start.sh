eeed31aec09b74b51c6b46bfceff7c15630b779c
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-23 16:05:41 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/maxsupun/VC-Streamer.git /VideoPlayerBot
-cd /VideoPlayerBot
-echo "Installing Requirements..."
-pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
-python3 main.py
